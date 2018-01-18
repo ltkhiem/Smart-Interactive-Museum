2a3daa4a135df607b7d97d8015f798e529c3790c
Che Huu <chequochuu@yahoo.com.vn>
2018-01-18 20:06:34 +0700

    fix some small syntax

--- recognize/urls.py
+++ recognize/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.recList, name='recList'),
+    path('', views.rec_list, name='recList'),
 ]
--- recognize/views.py
+++ recognize/views.py
@@ -6,20 +6,18 @@ from capstonemiddleware import ServerCallAPI
 # Create your views here.
 
 @csrf_exempt
-def recList(request):
-    if (request.method == 'POST'):
+def rec_list(request):
+    if request.method == 'POST':
         img = request.FILES['img']
         response = ServerCallAPI.request(img)
         if response[0] != 0:
             return HttpResponse('fail')
         else:
             code, labels, bounding_boxs = response
-            print(bounding_boxs)
-            res = {}
+            res = dict()
             res['results'] = []
             for i in range(labels.__len__()):
                 res['results'].append({"name": labels[i], "boundingbox": bounding_boxs[i]})
-            # res = {'results': [{"name": "chocam", "boundingbox": [1, 2, 3, 4]}, {"name": "chokhiem", "boundingbox": [5, 6, 7, 8]}]}
             res = json.dumps(res)
         return HttpResponse(res)
     else:
