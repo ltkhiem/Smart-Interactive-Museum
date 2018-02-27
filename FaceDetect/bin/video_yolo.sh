f5729ea627eecd9c69cab775562fc6b5740f5563
Che Huu <chequochuu@yahoo.com.vn>
2018-02-27 18:19:26 +0700

    delete weight and facenet, facedetect

--- FaceDetect/bin/video_yolo.sh
+++
@@ -1,3 +0,0 @@
-./darknet detector demo voc.names yolo-voc.cfg yolo-voc.weights -thresh 0.24 test.mp4
-
-
--- FaceDetect/bin/yolo.sh
+++
@@ -1,4 +0,0 @@
-./darknet detector test yolo-face.names yolo-face-test.cfg yolo-face.weights -thresh 0.24 stream crop
-
-
-
